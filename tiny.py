d782fe416cc637d39616bb7c09b63f4c9dc6696d
Viren070 <viren070@protonmail.com>
2024-04-14 16:29:21 +0100

    refactor: refactor tiny.py

--- tiny.py
+++ tiny.py
@@ -6,7 +6,56 @@ import datetime
 import logging
 import math
 
-logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s]: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s]: %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+VALID_FIELDS = ["bannerUrl", "category", "description", "developer", "frontBoxArt", "iconUrl", "id", "intro", "isDemo", "key", "languages", "name", "nsuId",
+                "numberOfPlayers", "publisher", "rank", "rating", "ratingContent", "region", "regions", "releaseDate", "rightsId", "screenshots", "size", "version"]
+
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+
+def validate_fields(fields):
+    if fields and not all(field in VALID_FIELDS for field in fields):
+        invalid_fields = ', '.join(set(fields) - set(VALID_FIELDS))
+        logging.error(f"Invalid field [{invalid_fields}]. Valid fields are {', '.join(VALID_FIELDS)}")
+        sys.exit(1)
+
+
+def read_json_file(file_path):
+    if not os.path.exists(file_path):
+        logging.error(f"File {file_path} does not exist")
+        sys.exit(1)
+
+    try:
+        with open(file_path) as f:
+            return json.load(f)
+    except json.JSONDecodeError as e:
+        logging.error(f"{file_path} is not a valid JSON file. {e}")
+        sys.exit(1)
+
+
+def reduce_data_size(data, fields):
+    new_data = {}
+    previous = 0
+    for count, (title_id, title_data) in enumerate(data.items(), start=1):
+        if title_data.get("name"):
+            new_data[title_id] = {field: title_data[field] for field in (fields or title_data) if field in title_data}
+
+        progress = int(math.floor(count/len(data)*100))
+        if progress != previous and progress % 5 == 0:
+            logging.info(f"Progress: {progress}% - {count}/{len(data)}")
+            previous = progress
+    return new_data
+
+
+def write_to_file(output, new_data):
+    try:
+        with open(output, "w") as f:
+            json.dump(new_data, f, separators=(",", ":"))
+    except Exception as e:
+        logging.error(f"Error writing to {output}. {e}")
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     logging.info("Initialising tiny.py")
@@ -21,48 +70,19 @@ if __name__ == "__main__":
     output = args.input_file if args.output_file is None else args.output_file
     fields = args.fields
 
-    valid_fields = ["bannerUrl", "category", "description", "developer", "frontBoxArt", "iconUrl", "id", "intro", "isDemo", "key", "languages", "name", "nsuId",
-                    "numberOfPlayers", "publisher", "rank", "rating", "ratingContent", "region", "regions", "releaseDate", "rightsId", "screenshots", "size", "version"]
+    validate_fields(fields)
 
-    if fields and not all(field in valid_fields for field in fields):
-        logging.error(f"Invalid field [{', '.join(set(fields) - set(valid_fields))}]. Valid fields are {', '.join(valid_fields)}")
-        sys.exit(1)
-
-    if not os.path.exists(args.input_file):
-        logging.error(f"File {args.input_file} does not exist")
-        sys.exit(1)
-
-    logging.info(f"Starting tiny.py with input file: {title_db}, output file: {output}, fields: {fields if fields else "All fields"}")
+    logging.info(f"Starting tiny.py with input file: {title_db}, output file: {output}, fields: {fields if fields else 'All fields'}")
     logging.info(f"Reading from {title_db}")
-    try:
-        with open(title_db) as f:
-            data = json.load(f)
-    except json.JSONDecodeError as e:
-        logging.error(f"{title_db} is not a valid JSON file. {e}")
-        sys.exit(1)
+    data = read_json_file(title_db)
 
     start_size = os.path.getsize(title_db)
     logging.info(f"Read {len(data)} titles from {title_db} ({start_size/1024/1024:.1f} MB)")
-    new_data = {}
-    previous = 0
-    for count, (title_id, title_data) in enumerate(data.items(), start=1):
-        # Skip titles without a name
-        if title_data.get("name"):
-            # Include only the specified fields if provided, otherwise include all fields
-            new_data[title_id] = {field: title_data[field] for field in (fields or title_data) if field in title_data}
 
-        progress = int(math.floor(count/len(data)*100))
-        if progress != previous and progress % 5 == 0:
-            logging.info(f"Progress: {progress}% - {count}/{len(data)}")
-            previous = progress
+    new_data = reduce_data_size(data, fields)
 
-    # write the new data to the output file
     logging.info(f"Writing {len(new_data)} titles to {output}")
-    try:
-        with open(output, "w") as f:
-            json.dump(new_data, f, separators=(",", ":"))
-    except Exception as e:
-        logging.error(f"Error writing to {output}. {e}")
-        sys.exit(1)
+    write_to_file(output, new_data)
+
     logging.info(f"Task completed in {(datetime.datetime.now() - start_time).total_seconds()} seconds and reduced the file size from {
                  (start_size/1024/1024):.1f} MB to {(os.path.getsize(output)/1024/1024):.1f} MB. Exiting.")
